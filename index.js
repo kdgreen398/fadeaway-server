38a99d40a27b52bbef61c89ca991055d8cf10c39
Kaleb Green <kdgreen398@gmail.com>
2023-10-30 23:54:35 -0500

    created authorization middleware

--- index.js
+++ index.js
@@ -3,25 +3,32 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const { verifyToken } = require("./util/jwt");
 const port = 3008;
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-  // main middleware function
+app.use(require("./controllers/authentication-controller"));
+app.use(require("./controllers/registration-controller"));
 
-  // console.log("-------LOG-------");
-  // console.log({
-  //   timestamp: new Date().toISOString(),
-  //   path: req.path,
-  //   queryParams: req.query,
-  // });
+app.use((req, res, next) => {
+  const token = req.get("auth-token");
+  if (!token) {
+    res.status(401).json({ error: "Unauthorized" });
+    return;
+  }
+
+  try {
+    verifyToken(token);
+  } catch {
+    res.status(401).json({ error: "Unauthorized" });
+    return;
+  }
+
   next();
 });
 
-app.use(require("./controllers/authentication-controller"));
-app.use(require("./controllers/registration-controller"));
 app.use(require("./controllers/recommendation-controller"));
 app.use(require("./controllers/location-controller"));
 app.use(require("./controllers/barber-controller"));

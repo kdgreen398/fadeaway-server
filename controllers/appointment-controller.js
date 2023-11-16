e14f3c99f2d74d75dcd8538b4a8f0ab3b9090916
Kaleb Green <kdgreen398@gmail.com>
2023-11-15 18:11:57 -0600

    added try catch to appointment controller

--- controllers/appointment-controller.js
+++ controllers/appointment-controller.js
@@ -7,12 +7,19 @@ const { verifyToken } = require("../util/jwt");
 router.get("/appointments/get-apppointments", async (req, res) => {
   logger.info("Entering Appointment Controller => get-apppointments");
 
-  const token = req.cookies["auth-token-server"];
-  const payload = verifyToken(token);
-  const appointments = await AppointmentService.getClientAppointments(payload);
+  try {
+    const token = req.cookies["auth-token-server"];
+    const payload = verifyToken(token);
+    const appointments = await AppointmentService.getClientAppointments(
+      payload,
+    );
+    res.send(appointments);
+  } catch (error) {
+    logger.error(error);
+    res.status(500).send(error.message);
+  }
 
   logger.info("Exiting Appointment Controller => get-apppointments");
-  res.send(appointments);
 });
 
 module.exports = router;

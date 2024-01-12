20deb8c2c16e60ea1f4090837171bb1524e9af37
Kaleb Green <kdgreen398@gmail.com>
2024-01-12 14:59:08 -0600

    can upload images to gcp bucket

--- src/controllers/image-controller.ts
+++ src/controllers/image-controller.ts
@@ -1,44 +1,96 @@
-import axios from "axios";
 import express, { Request, Response } from "express";
 import logger from "../util/logger";
 import { ResponseObject } from "../util/response-object";
 
+import { Storage } from "@google-cloud/storage";
+import multer from "multer";
+import sharp from "sharp";
+
+declare global {
+  namespace Express {
+    interface Request {
+      fileValidationError?: string;
+    }
+  }
+}
+
 const router = express.Router();
 
-// https://storage.googleapis.com/booking-app-image-bucket/tired%20steve.png
+const storage = new Storage();
 
-router.post("/image/upload", async (req: Request, res: Response) => {
-  logger.info("Entering Image Controller => upload");
+const bucket = storage.bucket(process.env.CLOUD_IMAGE_STORAGE_BUCKET || "");
 
-  // const { id } = req.query;
-
-  try {
-    res.json(ResponseObject.success("Image uploaded successfully"));
-  } catch (err: any) {
-    logger.error(err);
-    res.status(500).json(ResponseObject.error("Error uploading image"));
-  }
-  logger.info("Exiting Image Controller => upload");
+const multerStorage = multer.memoryStorage();
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype.startsWith("image/")) {
+      cb(null, true);
+    } else {
+      cb(new Error("Invalid file type. Please upload an image."));
+    }
+  },
 });
 
-router.get("/image/view/:imageName", async (req: Request, res: Response) => {
-  logger.info("Entering Image Controller => view");
+// Route for handling file upload
+router.post(
+  "/image/upload",
+  upload.single("file"),
+  async (req: Request, res: Response) => {
+    logger.info("Entering Image Controller => upload");
 
-  const { imageName } = req.params;
+    if (req.fileValidationError) {
+      return res
+        .status(400)
+        .json(ResponseObject.error(req.fileValidationError));
+    }
 
-  console.log(imageName);
+    const uploadedFile = req.file;
 
-  try {
-    const response = await axios(
-      "https://storage.googleapis.com/booking-app-image-bucket/tired%20steve.png",
-    );
-    // console.log(response);
-    res.json(ResponseObject.success(response.data));
-  } catch (err: any) {
-    logger.error(err);
-    res.status(500).json(ResponseObject.error("Error viewing image"));
-  }
-  logger.info("Exiting Image Controller => view");
-});
+    if (!uploadedFile) {
+      return res.status(400).json(ResponseObject.error("No file uploaded"));
+    }
+
+    try {
+      // // Resize the image using sharp
+      // const resizedImageBuffer = await sharp(uploadedFile.buffer)
+      //   .resize({ width: 400, height: 400, fit: "inside" }) // Maintain aspect ratio
+      //   .toBuffer();
+
+      // Use sharp to compress the image and save it as JPEG
+      const finalImageBuffer = await sharp(uploadedFile.buffer)
+        .jpeg({ quality: 60 }) // Set the desired quality (0-100)
+        .toBuffer();
+
+      console.log("Image compressed and saved as JPEG successfully.");
+      const fileName = Date.now() + "_" + uploadedFile.originalname;
+
+      const file = bucket.file(fileName);
+      const stream = file.createWriteStream({
+        metadata: {
+          contentType: uploadedFile.mimetype,
+        },
+      });
+
+      stream.end(finalImageBuffer);
+
+      stream.on("finish", () => {
+        // File uploaded successfully
+        res.json(
+          ResponseObject.success("Image uploaded successfully: " + fileName),
+        );
+      });
+
+      stream.on("error", (err) => {
+        console.error("Error uploading", err);
+        res.status(500).json(ResponseObject.error("Error uploading image"));
+      });
+    } catch (err: any) {
+      logger.error(err);
+      res.status(500).json(ResponseObject.error("Error uploading image"));
+    }
+    logger.info("Exiting Image Controller => upload");
+  },
+);
 
 export default router;
